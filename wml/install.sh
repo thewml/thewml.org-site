e750d907842fe37b72436993bbc641d371fd76ec
shlomif <shlomif@7081e830-ea04-0410-ae62-fb6148e2d9db>
2006-08-20 09:21:59 +0000

    Added a file to the install.

--- wml/install.sh
+++ wml/install.sh
@@ -1,6 +1,6 @@
 #!/usr/opkg/bin/bash
 
-find . -name '*.wml' -o -name '*.css' -o -name '*.pm' -o -name '*.pl' -o -name 'SNB' -o -name 'refs.db' -o -name 'Makefile' |
+find . -name '*.wml' -o -name '*.css' -o -name '*.pm' -o -name '*.pl' -o -name 'SNB' -o -name 'refs.db' -o -name 'Makefile' -o -name 'VERSION' |
 	(while read T ; do
 		if ! cmp -s "$T" /v/wml/web/"$T" ; then 
 			echo "Installing \"$T\""
